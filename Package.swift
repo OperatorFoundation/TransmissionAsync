0c64b6f13c528a19049c2070b0594906f69338cf
bragelbytes <joebragel@gmail.com>
2023-12-10 15:12:01 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"),
-
         .package(url: "https://github.com/OperatorFoundation/BlueSocket", branch: "release"),
         .package(url: "https://github.com/OperatorFoundation/Chord", branch: "release"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
