175a54e789db03741031ac217fb040a6a119b73e
bragelbytes <joebragel@gmail.com>
2023-12-14 18:30:26 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .executable(name: "TransmissionAsyncTester", targets: ["TransmissionAsyncTester"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
 
         .package(url: "https://github.com/OperatorFoundation/BlueSocket", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Chord", branch: "main"),
