40767f56f7e71593ce45b5f8e551fdba306587b4
consuelita <adelita@operatorfoundation.org>
2024-02-18 11:36:15 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// DO NOT UPDATE TO 5.8, IT BREAKS CONCURRENCY ON LINUX
 
 import PackageDescription
 
@@ -15,12 +14,12 @@ let package = Package(
         .executable(name: "TransmissionAsyncTester", targets: ["TransmissionAsyncTester"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
-        .package(url: "https://github.com/OperatorFoundation/BlueSocket", branch: "1.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Chord", branch: "0.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Straw", branch: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", branch: "1.2.6"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
+        .package(url: "https://github.com/OperatorFoundation/BlueSocket", from: "1.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
