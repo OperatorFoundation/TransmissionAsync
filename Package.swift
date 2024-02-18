60779c85421304ad88d6577c7055a5502e65b639
consuelita <adelita@operatorfoundation.org>
2024-02-18 13:03:27 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/OperatorFoundation/BlueSocket", from: "1.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/BlueSocket", from: "1.1.2"),
         .package(url: "https://github.com/OperatorFoundation/Chord", from: "0.1.1"),
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.1"),
         .package(url: "https://github.com/OperatorFoundation/Straw", from: "1.0.1"),
